e158505e15eb5d5b5a4335d1e02c36da618e62a9
Zebr <zebr_xie@163.com>
2016-09-11 23:21:16 +0800

    fix little bugs

--- app/bills/billsController.js
+++ app/bills/billsController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute','Algorithm'])
+angular.module('myApp.bills', ['ngRoute','Algorithm'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/bills', {
